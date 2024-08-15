ac5c28c5d87f76e0cb03222ab9ddc4f8342c3959
Ashish-Dobhal-Devops <154491535+Ashish-Dobhal-Devops@users.noreply.github.com>
2024-08-15 20:26:44 +0530

    Create vvvaaaariables.tf

---
+++ vvvaaaariables.tf
@@ -0,0 +1,65 @@
+variable "region" {
+  description = "The AWS region to deploy to"
+  default     = "us-east-1"
+}
+
+variable "hub_vpc_cidr" {
+  description = "CIDR block for the Hub VPC"
+  default     = "10.0.0.0/16"
+}
+
+variable "prod_vpc_cidr" {
+  description = "CIDR block for the Prod VPC"
+  default     = "10.1.0.0/16"
+}
+
+variable "availability_zones" {
+  description = "List of availability zones"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b"]
+}
+
+variable "hub_public_subnets" {
+  description = "Public subnets for the Hub VPC"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "hub_private_subnets" {
+  description = "Private subnets for the Hub VPC"
+  type        = list(string)
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
+}
+
+variable "hub_public_subnets_cidr" {
+  description = "CIDR blocks of public subnets for the Hub VPC"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "hub_private_subnets_cidr" {
+  description = "CIDR blocks of private subnets for the Hub VPC"
+  type        = list(string)
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
+}
+
+variable "prod_private_subnets" {
+  description = "Private subnets for the Prod VPC"
+  type        = list(string)
+  default     = ["10.1.1.0/24", "10.1.2.0/24"]
+}
+
+variable "ami_id" {
+  description = "AMI ID for EC2 instances"
+  default     = "ami-0c55b159cbfafe1f0"  # Example AMI ID
+}
+
+variable "instance_type" {
+  description = "Instance type for EC2 instances"
+  default     = "t2.micro"
+}
+
+variable "key_name" {
+  description = "Key name for EC2 instances"
+  default     = "my-key"
+}
